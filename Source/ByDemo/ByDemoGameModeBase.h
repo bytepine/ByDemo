495f65149fd4633f123c3c331618660ae72e760a
byteyang <bytepine@gmail.com>
2023-05-14 14:45:05 +0800

    Create Peoject

---
+++ Source/ByDemo/ByDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ByDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BYDEMO_API AByDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
